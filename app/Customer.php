d0cd21d1d526ad32f3b6b1e28d64f75e35acc5ea
Angga Gantira <angga.gantira@gmail.com>
2020-03-25 18:20:11 +0700

    Aplikasi E-Commerce Laravel 6 #12: Authorization & Customer Profile

--- app/Customer.php
+++ app/Customer.php
@@ -9,10 +9,16 @@ use Illuminate\Notifications\Notifiable;
 class Customer extends Authenticatable
 {
     use Notifiable;
+    
     protected $guarded = [];
 
     public function setPasswordAttribute($value)
     {
         $this->attributes['password'] = bcrypt($value);
+    }
+
+    public function district()
+    {
+        return $this->belongsTo(District::class);
     }
 }
